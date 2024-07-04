eb7c04a16ba8c6790b8accfa314a35eafb6fb52e
Revatdiyora <revatdiyora4@gmail.com>
2024-07-04 19:20:19 +0530

    Add files via upload

---
+++ data_type.php
@@ -0,0 +1,38 @@
+<?php
+
+$name = "Revat";
+$income = 20000;
+
+/* Data Type
+1 String
+2 Int
+3 Float
+4 Object
+5 Array
+6 Boolean
+7 Null
+*/
+
+//String
+$Name = "Revat";
+$frnd = "Chiku";
+echo "$name is Friend of $frnd";
+
+//Int
+$debt = -700;
+echo "$income";
+echo "$debt";
+
+//Float
+$income = 345.6;
+$debt = -56.8;
+echo "$income";
+echo "$debt";
+
+//Boolean
+$x = true;
+$is = false;
+echo $x;
+echo $is;
+
+?>
